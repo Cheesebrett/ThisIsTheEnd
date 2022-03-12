f928d3400494ef7e60a37398c185a8fba61a89b3
Cheesebrett <kleindaniel135@gmail.com>
2022-03-12 22:07:12 +0100

    Hail Mary

---
+++ guess_two_thirds/__init__.py
@@ -0,0 +1,101 @@
+from otree.api import *
+import numpy
+import itertools
+
+
+doc = """
+a.k.a. Ke123456ynesian beauty contest.
+Players all guess a number; whoever guesses closest to
+2/3 of the average wins.
+See https://en.wikipedia.org/wiki/Guess_2/3_of_the_average
+"""
+
+
+class C(BaseConstants):
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+    NAME_IN_URL = 'guess_two_thirds'
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    treatmentvideo = models.BooleanField()
+
+    guessx = models.IntegerField(
+        min=0, max=100, label="Please pick your first number from 0 to 100:"
+    )
+    guessy = models.IntegerField(
+        min=0, max=100, label="Please pick your second number from 0 to 100:"
+    )
+
+    internet = models.IntegerField(
+        label = "Was your internet connection stable enough to watch the video without trouble?",
+        choices=[
+            [1, 'No problems'],
+            [2, 'Some problems'],
+            [3, 'Could not really watch'],
+        ],
+        widget = widgets.RadioSelectHorizontal
+    )
+
+
+    cognitiveload = models.IntegerField(
+        label="How mentally demanding was the Instruction? Just answer quickly and intuitively.",
+        widget=widgets.RadioSelectHorizontal,
+        choices=[1, 'super easy to understand', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'extremely difficult to understand']
+    )
+
+
+# FUNCTIONS
+def creating_session(subsession):
+    import random
+    for player in subsession.get_players():
+        player.treatmentvideo = random.choice([True, False])
+        print('set treatment video to', player.treatmentvideo)
+
+
+def set_payoff(player: Player):
+    player.payoff = 1000-50*(abs(player.guessx-(2/3*player.guessx*player.guessy/2))+abs(player.guessy-(2/3*player.guessx*player.guessy/2)))
+
+
+# PAGES
+class Introduction(Page):
+    pass
+
+
+class Instruction1PGGVideo(Page):
+    pass
+
+
+class Instruction1PGGText(Page):
+    @staticmethod
+    def is_displayed(player: Player):
+        return player.treatmentvideo
+
+
+class QuestionInstruction(Page):
+    form_model = 'player'
+    form_fields = ['internet']
+
+
+class Guess(Page):
+    form_model = 'player'
+    form_fields = ['guessx', 'guessy']
+
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        player.set_payoff()
+
+
+class Results(Page):
+    pass
+
+
+page_sequence = [Introduction, Instruction1PGGVideo, QuestionInstruction, Guess, Results]
