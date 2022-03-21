6b54671eab7376fba67e3e96c0051877c71711e7
Cheesebrett <kleindaniel135@gmail.com>
2022-03-21 16:48:28 +0100

    Add new pages and other stuff

--- guess_two_thirds/__init__.py
+++ guess_two_thirds/__init__.py
@@ -1,7 +1,7 @@
 from otree.api import *
 import numpy
 import itertools
-
+import random
 
 doc = """
 a.k.a. Keynesian beauty contest.
@@ -15,7 +15,10 @@ class C(BaseConstants):
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
     NAME_IN_URL = 'guess_two_thirds'
-
+    Endowment = 1000
+    Punishment = 50
+    Correct_Instructions = 886
+    payment_instructions = 1
 
 class Subsession(BaseSubsession):
     pass
@@ -46,20 +49,44 @@ class Player(BasePlayer):
     )
 
     cognitiveload = models.IntegerField(
-        label = "How mentally demanding was the Instruction? Just answer quickly and intuitively."
+        label = "How mentally demanding was understanding the Instruction for the Task? Just answer quickly and intuitively."
     )
 
 
+    attention_check_instructions = models.IntegerField(
+        min=0, max=10000, label = "What is my favorite number between 0 and 10000? (This is just to see if you read the Instructions)"
+    )
+
+    gender = models.IntegerField(
+        label = "What is your gender?",
+        choices=[
+            [1, 'Male'],
+            [2, 'Female'],
+            [3, 'Other'],
+        ],
+    )
+
+    age = models.IntegerField(
+        min = 0, max = 100, label = "What is your age?"
+    )
+
+
+    math = models.IntegerField(widget=widgets.RadioSelect, label = "I am very good in math?", choices=[1, 2, 3, 4, 5])
+    english = models.IntegerField(widget=widgets.RadioSelect, label = "I speak English fluently?", choices=[1, 2, 3, 4, 5])
+    rhyme = models.IntegerField(widget=widgets.RadioSelect, label = "Tree rhymes with bee?", choices=[1, 2, 3, 4, 5])
+    familiarity = models.IntegerField(widget=widgets.RadioSelect, label = "I have been familiar with this task or a similar task before this experiment?", choices=[1, 2, 3, 4, 5])
+    offer_5 = models.IntegerField(widget=widgets.RadioSelect, choices=[1, 2, 3, 4, 5])
+
+    suggestions_box = models.StringField()
+
 # FUNCTIONS
 def creating_session(subsession):
-    import random
     for player in subsession.get_players():
         player.treatmentvideo = random.choice([True, False])
         print('set treatment video to', player.treatmentvideo)
 
-
-def set_payoff(player: Player):
-    player.payoff = 1000-50*(abs(player.guessx-(2/3*player.guessx*player.guessy/2))+abs(player.guessy-(2/3*player.guessx*player.guessy/2)))
+def ppg_payment(player):
+    player.ppg = 1000-50*(abs(player.guessx-(2/3*player.guessx*player.guessy/2))+abs(player.guessy-(2/3*player.guessx*player.guessy/2)))
 
 
 # PAGES
@@ -83,24 +110,36 @@ class QuestionInstruction(Page):
     @staticmethod
     def get_form_fields(player):
         if player.treatmentvideo:
-            return ['internet', 'cognitiveload']
+            return ['internet', 'cognitiveload', 'attention_check_instructions']
         else:
-            return ['cognitiveload']
+            return ['cognitiveload', 'attention_check_instructions']
 
-
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        if player.attention_check_instructions == C.Correct_Instructions:
+            player.payoff = C.payment_instructions
+        else:
+            player.payoff = 0
 
 
 class Guess(Page):
     form_model = 'player'
     form_fields = ['guessx', 'guessy']
+    timeout_seconds = 60
+    timer_text = 'Time left to make a decision!'
 
     @staticmethod
     def before_next_page(player, timeout_happened):
-        player.set_payoff()
+        if timeout_happened:
+            player.guessx = 100
+            player.guessy = 100
 
+class Survey(Page):
+    form_model = 'player'
+    form_fields = ['gender', 'age', 'math', 'english', 'rhyme', 'familiarity', 'offer_5', 'suggestions_box']
 
 class Results(Page):
     pass
 
 
-page_sequence = [Introduction, Instruction1PGGVideo, QuestionInstruction, Guess, Results]
+page_sequence = [Introduction, Instruction1PGGVideo, QuestionInstruction, Guess, Survey, Results]
